15e87191b30b50b8952e8c72fddc9247dfdbf0c2
Mikaela Hedberg <mie@du.se>
2025-08-21 14:53:46 +0200

    reference material for FL2

--- 47/FL/script.js
+++ 47/FL/script.js
@@ -8,17 +8,30 @@ console.log(document.querySelector('#menu'));
 console.log(document.querySelector('.menu_item'));
 
 const listELements = document.querySelectorAll('#menu ul');
-const menuItems = document.querySelectorAll('.menu_item');
-
 const list = listELements[0];
-console.log(list);
-const menuItem = menuItems[0];
+const menuItemNodes = document.querySelectorAll('.menu_item');
+const menuItems = document.getElementsByClassName('menu_item');
 
+const menuItem = menuItems[0];
 menuItem.style.backgroundColor = 'yellow';
 menuItem.classList.add('error');
 
-const newElement = document.createElement('li');
+const newElement = document.createElement('li'); // har inget innehåll
+newElement.innerHTML = 'Item 4';
+newElement.classList.add('menu_item');
 newElement.style.backgroundColor = 'green';
-list.append(newElement);
-list.insertAdjacentElement('beforebegin', newElement);
-list.insertAdjacentElement('beforeend', newElement);
+
+const newElementString = `<li style="background-color: pink;"class="menu_item error">Item 5</li>`;
+
+list.insertAdjacentElement('afterbegin', newElement);
+list.insertAdjacentHTML('beforeend', newElementString);
+
+for (let menuItem of menuItems) {
+  menuItem.style.backgroundColor = 'white';
+  menuItem.classList.add('error');
+}
+
+menuItemNodes.forEach((item) => {
+  item.style.backgroundColor = 'cyan';
+  item.classList.remove('error');
+});
