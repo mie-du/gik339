b0a5e77b7fcbc3eeb446ed52b0222839b398f3d1
Mikaela Hedberg <mie@du.se>
2025-10-06 12:56:16 +0200

    w 50

--- 49/FL/server-express.js
+++ 49/FL/server-express.js
@@ -10,13 +10,10 @@ server.listen(3000, () => {
 server.get('/', (req, res) => {
   const db = new sqlite3.Database(':memory:');
 
-  db.serialize(() => {
-    db.run('CREATE TABLE greetings (message TEXT)');
-    db.run('INSERT INTO greetings (message) VALUES (?)', ['Hej, SQLite!']);
-  });
-
-  db.get('SELECT message FROM greetings', (err, row) => {
-    res.send(row.message);
+  db.run('CREATE TABLE IF NOT EXISTS greetings (message TEXT)');
+  db.run('INSERT INTO greetings (message) VALUES (?)', ['Hej, SQLite!']);
+  db.all('SELECT message FROM greetings', (err, row) => {
+    res.send(JSON.stringify(row));
     db.close();
   });
 });
---
+++ 50/FL/script.js
@@ -0,0 +1,41 @@
+const url = 'http://localhost:3000';
+fetch(`${url}`)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    data.forEach((item) => {
+      const paragraph = document.createElement('p');
+      paragraph.innerHTML = item.message;
+      document.body.insertAdjacentElement('beforeend', paragraph);
+    });
+  });
+
+fetch(`${url}/users`)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    const paragraph = document.createElement('p');
+    paragraph.innerHTML = data.message;
+    document.body.insertAdjacentElement('beforeend', paragraph);
+  });
+
+const form = document.getElementById('exampleForm');
+form.addEventListener('submit', handleSubmit);
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const form = e.target;
+  const field = form.inputField;
+  const serverObject = { message: field.value };
+  console.log(serverObject);
+
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(serverObject),
+    headers: {
+      'Content-Type': 'Application/json'
+    }
+  });
+}
