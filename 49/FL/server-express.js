52122d9ad63f559d39870963856b91f2db0a21ae
Mikaela Hedberg <mie@du.se>
2026-09-21 12:33:06 +0200

    changed to better-sqlite

--- 49/FL/server-express.js
+++ 49/FL/server-express.js
@@ -1,22 +1,19 @@
 const express = require('express');
-const sqlite3 = require('sqlite').verbose();
-
 const server = express();
 
 server.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
 
+const Database = require('better-sqlite3');
+
 server.get('/', (req, res) => {
-  const db = new sqlite3.Database(':memory:');
+  const db = new Database(':memory:');
 
-  db.serialize(() => {
-    db.run('CREATE TABLE IF NOT EXISTS greetings (message TEXT)');
-    db.run('INSERT INTO greetings (message) VALUES (?)', ['Hej, SQLite!']);
-  });
+  db.exec('CREATE TABLE IF NOT EXISTS greetings (message TEXT)');
+  db.prepare('INSERT INTO greetings (message) VALUES (?)').run('Hej, SQLite!');
 
-  db.get('SELECT message FROM greetings', (err, row) => {
-    res.send(row);
-    db.close();
-  });
+  const row = db.prepare('SELECT message FROM greetings').get();
+  res.send(row);
+  db.close();
 });
